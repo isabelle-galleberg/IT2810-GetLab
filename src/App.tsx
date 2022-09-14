1399c95f6726bb48195cea31e043b515f40c94d5
Ole Marius Smiseth Inderhaug <olein@stud.ntnu.no>
2022-09-14 10:30:23 +0200

    feat: create layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import "./App.css";
+import { LayoutProps } from "./types/propTypes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props: LayoutProps) {
+	return (
+		<div className="container">
+			<header>
+				<img className="getLabLogo" src={require("./assets/logo.png")} alt="" />
+				<h1>Get your GitLab data with GetLab!</h1>
+			</header>
+			<main></main>
+			<footer>
+				<p>
+					Created with <span className="heart">&hearts;</span> by Ole, Axel, Eva
+					and Isabelle
+				</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
