93cc8c784641a77e0cde3de665a5dd1410624888
evateis <evateisb@gmail.com>
2022-09-23 12:21:15 +0200

    feat: create text field component

---
+++ src/components/textField/TextField.tsx
@@ -0,0 +1,64 @@
+import "./TextField.css"
+import React, { useState } from "react"
+
+interface Props {
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function FormInput({ onChange, placeholder }: Props) {
+  return (
+    <div className="wrapperInput">
+      <input className="inputField" onChange={onChange} placeholder={placeholder} ></input>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { TextField } from "@material-ui/core";
+
+// type CustomTextFieldProps = {
+//   label: string,
+//   name: string,
+//   changeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void,
+// }
+
+// const CustomTextField = (props: CustomTextFieldProps) => {
+//   return (
+//     <TextField
+//       label={props.label}
+//       name={props.name}
+//       onChange={props.changeHandler}
+
+//       variant={"outlined"} //enables special material-ui styling
+//       size={"small"}
+//       margin={"dense"}
+//     />
+//   );
+// }
