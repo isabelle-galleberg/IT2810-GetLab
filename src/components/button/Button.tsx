a6f3a189e564268e8afda999c43463b8a920d905
evateis <evateisb@gmail.com>
2022-09-23 12:20:49 +0200

    feat: create button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./Button.css"
+
+interface Props {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children?: React.ReactNode;
+  disabled?: boolean;
+};
+
+export default function Button({ onClick, children, disabled }: Props) {
+  return (
+    <button className="button" onClick={onClick} disabled={disabled} >
+      {children}
+    </button >
+  );
+}
