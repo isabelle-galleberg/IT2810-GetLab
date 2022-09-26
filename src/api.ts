9876ee6535f54e6153b78402d5747e97e1ebc163
Axel Legallais-Korsbakken <axelle@stud.ntnu.no>
2022-09-26 10:27:41 +0200

    make api call async

--- src/api.ts
+++ src/api.ts
@@ -1,47 +1,37 @@
-export function getBranches(projecID: number): Promise<unknown> {
+export async function getBranches(projecID: number): Promise<unknown> {
 
-    return fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/branches')
-        .then(response => response.json())
-        .then(response => {
-            return response as unknown;
-        })
+    const response = await fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/branches');
+    const data = await response.json();
+    return data;
 }
 
-export function getAllCommits(projecID: number): Promise<unknown> {
+export async function getAllCommits(projecID: number): Promise<unknown> {
 
-    return fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/commits')
-        .then(response => response.json())
-        .then(response => {
-            return response as unknown;
-        })
+    const response = await fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/commits');
+    const data = await response.json();
+    return data;
 }
 
-export function getCommitsByBranch(projecID: number, branchName: string): Promise<unknown> {
+export async function getCommitsByBranch(projecID: number, branchName: string): Promise<unknown> {
 
-    return fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/commits?ref_name=' + branchName)
-        .then(response => response.json())
-        .then(response => {
-            return response as unknown;
-        })
+    const response = await fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/commits?ref_name=' + branchName);
+    const data = await response.json();
+    return data;
 }
 
 
-export function getAllIssues(projecID: number): Promise<unknown> {
+export async function getAllIssues(projecID: number): Promise<unknown> {
 
-    return fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/issues')
-        .then(response => response.json())
-        .then(response => {
-            return response as unknown;
-        })
+    const response = await fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/issues');
+    const data = await response.json();
+    return data;
 }
 
-export function getIssuesByLabels(projecID: number, labelList: string[]): Promise<unknown> {
+export async function getIssuesByLabels(projecID: number, labelList: string[]): Promise<unknown> {
 
     const labelString = labelList.join(',');
 
-    return fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/issues?labels=' + labelString)
-        .then(response => response.json())
-        .then(response => {
-            return response as unknown;
-        })
+    const response = await fetch('https://gitlab.com/api/v4/projects/' + projecID.toString() +'/repository/issues?labels=' + labelString);
+    const data = await response.json();
+    return data;
 }
