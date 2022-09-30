6952ff1354bb55ab37e8f7ee94ad419694109f7e
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-09-30 13:15:47 +0200

    fix: display commit author email instead of name

--- src/components/CommitsChart/CommitsChart.tsx
+++ src/components/CommitsChart/CommitsChart.tsx
@@ -1,60 +1,61 @@
 //Inspired by https://react-chartjs-2.js.org/examples/vertical-bar-chart/
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import "./CommitsChart.css";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import commitService from "../../services/commitServices";
 import { CommitsChartProps } from "../../types/propTypes";
 
 export default function Chart({ projectId, token }: CommitsChartProps) {
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+	ChartJS.register(
+		CategoryScale,
+		LinearScale,
+		BarElement,
+		Title,
+		Tooltip,
+		Legend
+	);
 
-  const [contributors, setContributors] = useState<string[]>([]);
-  const [commits, setCommits] = useState<number[]>([]);
+	const [contributors, setContributors] = useState<string[]>([]);
+	const [commits, setCommits] = useState<number[]>([]);
 
-  const data = {
-    labels: contributors,
-    datasets: [
-      {
-        label: "Commits",
-        data: commits,
-        backgroundColor: "rgba(245, 101, 39, 0.8)",
-      },
-    ],
-  };
+	const data = {
+		labels: contributors.map((c) => c.split("@")[0]),
+		datasets: [
+			{
+				label: "Commits",
+				data: commits,
+				backgroundColor: "rgba(245, 101, 39, 0.8)",
+			},
+		],
+	};
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top" as const,
-      },
-    },
-  };
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "top" as const,
+			},
+		},
+	};
 
-  useEffect(() => {
-    commitService
-      .getCommitsPerAuthor(projectId, token)
-      .then((commitsPerAuthor: Map<string, number>) => {
-        setContributors(Array.from(commitsPerAuthor.keys()));
-        setCommits(Array.from(commitsPerAuthor.values()));
-      });
-  }, []);
+	useEffect(() => {
+		commitService
+			.getCommitsPerAuthor(projectId, token)
+			.then((commitsPerAuthor: Map<string, number>) => {
+				setContributors(Array.from(commitsPerAuthor.keys()));
+				setCommits(Array.from(commitsPerAuthor.values()));
+			});
+	}, []);
 
-  return <Bar data={data} options={options} />;
+	return <Bar data={data} options={options} className="commitsChart" />;
 }
--- src/services/commitServices.ts
+++ src/services/commitServices.ts
@@ -1,67 +1,78 @@
 // Returns the response of an api call to get the commits of a project
 async function getAllCommits(
-  projectId: string,
-  privateToken: string
+	projectId: string,
+	privateToken: string
 ): Promise<any> {
-  try {
-    const response = await fetch(
-      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
-        projectId +
-        "/repository/commits?private_token=" +
-        privateToken
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const response = await fetch(
+			"https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+				projectId +
+				"/repository/commits?private_token=" +
+				privateToken
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 // Returns the response of an api call to get the commits of a given branch
 async function getCommitsByBranch(
-  projectId: string,
-  branchName: string,
-  privateToken: string
+	projectId: string,
+	branchName: string,
+	privateToken: string
 ): Promise<any> {
-  try {
-    const response = await fetch(
-      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
-        projectId +
-        "/repository/commits?ref_name=" +
-        branchName +
-        "&private_token=" +
-        privateToken
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const response = await fetch(
+			"https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+				projectId +
+				"/repository/commits?ref_name=" +
+				branchName +
+				"&private_token=" +
+				privateToken
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 // Returns a map of how many commits each user has made
-async function getCommitsPerAuthor(projectId: string, privateToken: string): Promise<any> {
-    try {
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId +'/repository/commits?ref_name=main&per_page=100&private_token=' + privateToken);
-        const data = await response.json();
-        let commitsPerAuthor = new Map<string, number>();
-        for (const commit of data){
-            if (commitsPerAuthor.has(commit.author_name)){
-                commitsPerAuthor.set(commit.author_name, commitsPerAuthor.get(commit.author_name)! + 1);
-            } else {
-                commitsPerAuthor.set(commit.author_name, 1);
-            }
-        }
-        return commitsPerAuthor;
-    } catch (error) {
-        console.log(error);
-    }
+async function getCommitsPerAuthor(
+	projectId: string,
+	privateToken: string
+): Promise<any> {
+	try {
+		const response = await fetch(
+			"https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+				projectId +
+				"/repository/commits?ref_name=main&per_page=100&private_token=" +
+				privateToken
+		);
+		const data = await response.json();
+		let commitsPerAuthor = new Map<string, number>();
+		for (const commit of data) {
+			if (commitsPerAuthor.has(commit.committer_email)) {
+				commitsPerAuthor.set(
+					commit.committer_email,
+					commitsPerAuthor.get(commit.committer_email)! + 1
+				);
+			} else {
+				commitsPerAuthor.set(commit.committer_email, 1);
+			}
+		}
+		return commitsPerAuthor;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 const commitService = {
-    getAllCommits,
-    getCommitsByBranch,
-    getCommitsPerAuthor
-}
+	getAllCommits,
+	getCommitsByBranch,
+	getCommitsPerAuthor,
+};
 
 export default commitService;
