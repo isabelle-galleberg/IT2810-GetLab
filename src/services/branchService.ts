a10feea33a06e1bff5183b84aec8b8877078a8e6
Axel Legallais-Korsbakken <axelle@stud.ntnu.no>
2022-09-30 13:57:17 +0200

    feat: be able to fetch all commits/issues/branches

--- src/services/branchService.ts
+++ src/services/branchService.ts
@@ -1,16 +1,27 @@
-// Returns the response of an api call to get the branches of a project
+// Returns the response of an api call to get the branches of a project. Iterates through all pages.
 async function getBranches(
   projectId: string,
   privateToken: string
 ): Promise<any> {
   try {
-    const response = await fetch(
-      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
-        projectId +
-        "/repository/branches?private_token=" +
-        privateToken
-    );
-    const data = await response.json();
+    // const data = await response.json();
+    let data: any[] = [];
+    let response_size = 100;
+    let page = 1;
+    while (response_size == 100) {
+      const response = await fetch(
+        "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+          projectId +
+          "/repository/branches?per_page=100&private_token=" +
+          privateToken +
+          "&page=" +
+          page
+      );
+      const response_data = await response.json();
+      response_size = response_data.length;
+      data = data.concat(response_data);
+      page++;
+    }
     return data;
   } catch (error) {
     console.log(error);
--- src/services/commitServices.ts
+++ src/services/commitServices.ts
@@ -1,67 +1,108 @@
-// Returns the response of an api call to get the commits of a project
+// Returns the response of an api call to get the commits of a project. Iterates through all pages.
 async function getAllCommits(
   projectId: string,
   privateToken: string
 ): Promise<any> {
   try {
-    const response = await fetch(
-      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
-        projectId +
-        "/repository/commits?private_token=" +
-        privateToken
-    );
-    const data = await response.json();
+    let data: any[] = [];
+    let response_size = 100;
+    let page = 1;
+    while (response_size == 100) {
+      const response = await fetch(
+        "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+          projectId +
+          "/repository/commits?per_page=100&private_token=" +
+          privateToken +
+          "&page=" +
+          page
+      );
+      const response_data = await response.json();
+      response_size = response_data.length;
+      data = data.concat(response_data);
+      page++;
+    }
     return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-// Returns the response of an api call to get the commits of a given branch
+// Returns the response of an api call to get the commits of a given branch. Iterates through all pages.
 async function getCommitsByBranch(
   projectId: string,
   branchName: string,
   privateToken: string
 ): Promise<any> {
   try {
-    const response = await fetch(
-      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
-        projectId +
-        "/repository/commits?ref_name=" +
-        branchName +
-        "&private_token=" +
-        privateToken
-    );
-    const data = await response.json();
+    let data: any[] = [];
+    let response_size = 100;
+    let page = 1;
+    while (response_size == 100) {
+      const response = await fetch(
+        "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+          projectId +
+          "/repository/commits?ref_name=" +
+          branchName +
+          "&per_page=3&private_token=" +
+          privateToken +
+          "&page=" +
+          page
+      );
+      const response_data = await response.json();
+      response_size = response_data.length;
+      data = data.concat(response_data);
+      page++;
+    }
     return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-// Returns a map of how many commits each user has made
-async function getCommitsPerAuthor(projectId: string, privateToken: string): Promise<any> {
-    try {
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId +'/repository/commits?ref_name=main&per_page=100&private_token=' + privateToken);
-        const data = await response.json();
-        let commitsPerAuthor = new Map<string, number>();
-        for (const commit of data){
-            if (commitsPerAuthor.has(commit.author_name)){
-                commitsPerAuthor.set(commit.author_name, commitsPerAuthor.get(commit.author_name)! + 1);
-            } else {
-                commitsPerAuthor.set(commit.author_name, 1);
-            }
-        }
-        return commitsPerAuthor;
-    } catch (error) {
-        console.log(error);
+// Returns a map of how many commits each user has made. Iterates through all pages.
+async function getCommitsPerAuthor(
+  projectId: string,
+  privateToken: string
+): Promise<any> {
+  try {
+    let data: any[] = [];
+    let response_size = 100;
+    let page = 1;
+    while (response_size == 100) {
+      const response = await fetch(
+        "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+          projectId +
+          "/repository/commits?ref_name=main&per_page=100&private_token=" +
+          privateToken +
+          "&page=" +
+          page
+      );
+      const response_data = await response.json();
+      response_size = response_data.length;
+      data = data.concat(response_data);
+      page++;
     }
+    let commitsPerAuthor = new Map<string, number>();
+    for (const commit of data) {
+      if (commitsPerAuthor.has(commit.author_name)) {
+        commitsPerAuthor.set(
+          commit.author_name,
+          commitsPerAuthor.get(commit.author_name)! + 1
+        );
+      } else {
+        commitsPerAuthor.set(commit.author_name, 1);
+      }
+    }
+    return commitsPerAuthor;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const commitService = {
-    getAllCommits,
-    getCommitsByBranch,
-    getCommitsPerAuthor
-}
+  getAllCommits,
+  getCommitsByBranch,
+  getCommitsPerAuthor,
+};
 
 export default commitService;
