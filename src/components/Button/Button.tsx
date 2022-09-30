fc38e38f7a2e292486920634c1bee66ef9e1c234
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-09-30 12:18:05 +0200

    chore: format with Prettier

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,17 +3,17 @@ import { ButtonProps } from "../../types/propTypes";
 import "./Button.css";
 
 class Button extends React.Component<ButtonProps> {
-	render() {
-		return (
-			<button
-				className="button"
-				onClick={this.props.onClick}
-				disabled={this.props.disabled}
-			>
-				{this.props.children}
-			</button>
-		);
-	}
+  render() {
+    return (
+      <button
+        className="button"
+        onClick={this.props.onClick}
+        disabled={this.props.disabled}
+      >
+        {this.props.children}
+      </button>
+    );
+  }
 }
 
 export default Button;
--- src/services/issueService.ts
+++ src/services/issueService.ts
@@ -1,48 +1,81 @@
 // Returns the response of an api call to get the issues of a project and how many pages the response has
-async function getIssues(projectId: string, privateToken: string, numberOfIssues: string, page: number): Promise<any> {
-    try {
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId + '/issues?per_page=' + numberOfIssues + '&private_token=' + privateToken + "&page=" + page);
-        const data = await response.json();
-        const totalPages = response.headers.get("X-Total-Pages");
-        return {data, totalPages};
-    } catch (error) {
-        console.log(error);
-    }
+async function getIssues(
+  projectId: string,
+  privateToken: string,
+  numberOfIssues: string,
+  page: number
+): Promise<any> {
+  try {
+    const response = await fetch(
+      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+        projectId +
+        "/issues?per_page=" +
+        numberOfIssues +
+        "&private_token=" +
+        privateToken +
+        "&page=" +
+        page
+    );
+    const data = await response.json();
+    const totalPages = response.headers.get("X-Total-Pages");
+    return { data, totalPages };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Returns the response of an api call to get the issues with a set of given labels
-async function getIssuesByLabels(projectId: string, labelList: string[], privateToken: string): Promise<any> {
-    try {
-        const labelString = labelList.join(',');
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId +'/issues?labels=' + labelString + '&private_token=' + privateToken);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+async function getIssuesByLabels(
+  projectId: string,
+  labelList: string[],
+  privateToken: string
+): Promise<any> {
+  try {
+    const labelString = labelList.join(",");
+    const response = await fetch(
+      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+        projectId +
+        "/issues?labels=" +
+        labelString +
+        "&private_token=" +
+        privateToken
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Returns a list of all labelnames in a project
-async function getLabels(projectId: string, privateToken: string): Promise<any> {
-    try {
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId +'/labels?private_token=' + privateToken);
-        const data = await response.json();
-        let labelList: string[] = [];
-        for (const label of data){
-            if (!labelList.includes(label.name)){
-                labelList.push(label.name);
-            }
-        }
-        return labelList;
-    } catch (error) {
-        console.log(error);
+async function getLabels(
+  projectId: string,
+  privateToken: string
+): Promise<any> {
+  try {
+    const response = await fetch(
+      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+        projectId +
+        "/labels?private_token=" +
+        privateToken
+    );
+    const data = await response.json();
+    let labelList: string[] = [];
+    for (const label of data) {
+      if (!labelList.includes(label.name)) {
+        labelList.push(label.name);
+      }
     }
-}   
+    return labelList;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const issueService = {
-    getIssues,
-    getIssuesByLabels,
-    getLabels
-}
+  getIssues,
+  getIssuesByLabels,
+  getLabels,
+};
 
 export default issueService;
