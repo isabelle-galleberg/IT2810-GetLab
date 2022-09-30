3c8dcc88a8c5d2faa0fae50fa40435c604085b47
Isabelle Galleberg <isabeg@stud.ntnu.no>
2022-09-30 14:37:44 +0200

    chore: install Prettier

--- src/services/commitServices.ts
+++ src/services/commitServices.ts
@@ -40,28 +40,39 @@ async function getCommitsByBranch(
 }
 
 // Returns a map of how many commits each user has made
-async function getCommitsPerAuthor(projectId: string, privateToken: string): Promise<any> {
-    try {
-        const response = await fetch('https://gitlab.stud.idi.ntnu.no/api/v4/projects/' + projectId +'/repository/commits?ref_name=main&per_page=100&private_token=' + privateToken);
-        const data = await response.json();
-        let commitsPerAuthor = new Map<string, number>();
-        for (const commit of data){
-            if (commitsPerAuthor.has(commit.author_name)){
-                commitsPerAuthor.set(commit.author_name, commitsPerAuthor.get(commit.author_name)! + 1);
-            } else {
-                commitsPerAuthor.set(commit.author_name, 1);
-            }
-        }
-        return commitsPerAuthor;
-    } catch (error) {
-        console.log(error);
+async function getCommitsPerAuthor(
+  projectId: string,
+  privateToken: string
+): Promise<any> {
+  try {
+    const response = await fetch(
+      "https://gitlab.stud.idi.ntnu.no/api/v4/projects/" +
+        projectId +
+        "/repository/commits?ref_name=main&per_page=100&private_token=" +
+        privateToken
+    );
+    const data = await response.json();
+    let commitsPerAuthor = new Map<string, number>();
+    for (const commit of data) {
+      if (commitsPerAuthor.has(commit.author_name)) {
+        commitsPerAuthor.set(
+          commit.author_name,
+          commitsPerAuthor.get(commit.author_name)! + 1
+        );
+      } else {
+        commitsPerAuthor.set(commit.author_name, 1);
+      }
     }
+    return commitsPerAuthor;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const commitService = {
-    getAllCommits,
-    getCommitsByBranch,
-    getCommitsPerAuthor
-}
+  getAllCommits,
+  getCommitsByBranch,
+  getCommitsPerAuthor,
+};
 
 export default commitService;
