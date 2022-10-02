6d19507d86d1438671dced44cc6c17b50a7100ac
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-02 19:13:08 +0200

    test: snapshot test for issue cards

---
+++ src/tests/IssueCard.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import IssueCard from "../components/IssueCard/IssueCard";
+
+describe("<IssueCard />", () => {
+  it("should render an issue card with given values", () => {
+    const elem = renderer
+      .create(
+        <IssueCard
+          title="Make it work"
+          labels={["priority: high"]}
+          issueNumber={1}
+          createdAt="10.08.2022"
+          author="me"
+        />
+      )
+      .toJSON();
+    expect(elem).toMatchSnapshot();
+  });
+});
