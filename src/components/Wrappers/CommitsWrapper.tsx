52414943ade5b6d5876e1f8f647080f7ed39bc49
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-04 18:11:22 +0200

    test: commit filtering by date

--- src/components/Wrappers/CommitsWrapper.tsx
+++ src/components/Wrappers/CommitsWrapper.tsx
@@ -8,47 +8,40 @@ export default function CommitsWrapper({ pageinator, setPageinator }: any) {
   const [commits, setCommits] = useState<any[]>([]);
   const [branches, setBranches] = useState<any[]>([]);
   const [filter, setFilter] = useState<any>({
-    branch: ""
+    branch: "",
   });
   const [dateRange, setDateRange] = useState<any>({
     dateFrom: "",
-    dateTo: ""
+    dateTo: "",
   });
   var count = 0;
 
   useEffect(() => {
     setCommits([]);
     commitService
-      .getCommitsByBranch(
-        "17379",
-        filter.branch,
-        "glpat-GPrQJsa8_WicT1Fo5Ve1"
-      )
+      .getCommitsByBranch("17379", filter.branch, "glpat-GPrQJsa8_WicT1Fo5Ve1")
       .then((res: any) => {
         res.map((data: any) => {
           if (
-            validDate(data.committed_date.slice(0, 10), dateRange.dateFrom, dateRange.dateTo) ||
+            validDate(
+              data.committed_date.slice(0, 10),
+              dateRange.dateFrom,
+              dateRange.dateTo
+            ) ||
             dateRange.dateFrom === ""
           ) {
             count = count + 1;
             setCommits((commits) => [...commits, data]);
             setPageinator(null, count);
           }
-        })
+        });
       });
   }, [filter, dateRange]);
 
-  function validDate(commitDate: string, fromDate: string, toDate: string) {
-    var date = Date.parse(commitDate);
-    var from = Date.parse(fromDate);
-    var to = Date.parse(toDate);
-    if (from <= date && date <= to) {
-      return true;
-    }
-    return false;
-  }
-
-  const commitsPerPage = commits.slice((pageinator.page - 1) * pageinator.perPage, pageinator.page * pageinator.perPage);
+  const commitsPerPage = commits.slice(
+    (pageinator.page - 1) * pageinator.perPage,
+    pageinator.page * pageinator.perPage
+  );
 
   return (
     <div>
@@ -60,23 +53,35 @@ export default function CommitsWrapper({ pageinator, setPageinator }: any) {
           setFilter={setFilter}
           dateRange={dateRange}
           setDateRange={setDateRange}
-          onChange={pageinator.page = 1}
+          onChange={(pageinator.page = 1)}
         ></CommitFilter>
       </div>
       <div className="commitCards">
-        {commitsPerPage
-          .map((res: any) => {
-            return (
-              <CommitCard
-                key={res.id}
-                title={res.title}
-                committedAt={res.committed_date.slice(0, 10)}
-                author={res.author_name}
-              />
-            );
-          })
-        }
+        {commitsPerPage.map((res: any) => {
+          return (
+            <CommitCard
+              key={res.id}
+              title={res.title}
+              committedAt={res.committed_date.slice(0, 10)}
+              author={res.author_name}
+            />
+          );
+        })}
       </div>
     </div>
   );
+}
+
+export function validDate(
+  commitDate: string,
+  fromDate: string,
+  toDate: string
+) {
+  var date = Date.parse(commitDate);
+  var from = Date.parse(fromDate);
+  var to = Date.parse(toDate);
+  if (from <= date && date <= to) {
+    return true;
+  }
+  return false;
 }
