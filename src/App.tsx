81c630d10994b9e42d9c57316ebe65628b35a1ab
Ole Marius Smiseth Inderhaug <olein@stud.ntnu.no>
2022-10-04 13:41:30 +0200

    feat: show repo name in corner

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,48 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
 import StartPage from "./pages/StartPage/StartPage";
 import { GitlabContext, GitlabCredentials } from "./context/GitlabContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import branchService from "./services/branchService";
+import { getRepositoryName } from "./services/repositoryServices";
 function App() {
   const [projectId, setProjectId] = useState<string>("");
   const [accessToken, setAccessToken] = useState<string>("");
+  const [repositoryName, setRepositoryName] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (projectId && accessToken) {
+      getRepositoryName(projectId, accessToken).then((name) => {
+        setRepositoryName(name);
+      });
+    }
+  }, [accessToken, projectId]);
+
   return (
-    <div className="container">
-      <header>
-        <a href="/">
-          <img
-            className="getLabLogo"
-            src={require("./assets/logo.png")}
-            alt=""
-          />
-        </a>
-        <h1>Get your GitLab data with GetLab!</h1>
-      </header>
-      <main>
-        <GitlabContext.Provider
-          value={
-            {
-              projectId,
-              setProjectId,
-              accessToken: accessToken,
-              setAccessToken: setAccessToken,
-            } as GitlabCredentials
-          }
-        >
+    <GitlabContext.Provider
+      value={
+        {
+          projectId,
+          setProjectId,
+          accessToken: accessToken,
+          setAccessToken: setAccessToken,
+        } as GitlabCredentials
+      }
+    >
+      <div className="container">
+        <header>
+          <a href="/">
+            <img
+              className="getLabLogo"
+              src={require("./assets/logo.png")}
+              alt=""
+            />
+          </a>
+          <div className="headerContainer">
+            <h1>Get your GitLab data with GetLab!</h1>
+            {repositoryName && <h1>Now showing: {repositoryName}</h1>}
+          </div>
+        </header>
+        <main>
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<StartPage />} />
@@ -38,15 +53,15 @@ function App() {
               <Route path="/data" element={<MainPage />} />
             </Routes>
           </BrowserRouter>
-        </GitlabContext.Provider>
-      </main>
-      <footer>
-        <p>
-          Created with <span className="heart">&hearts;</span> by Ole, Axel, Eva
-          and Isabelle
-        </p>
-      </footer>
-    </div>
+        </main>
+        <footer>
+          <p>
+            Created with <span className="heart">&hearts;</span> by Ole, Axel,
+            Eva and Isabelle
+          </p>
+        </footer>
+      </div>
+    </GitlabContext.Provider>
   );
 }
 
