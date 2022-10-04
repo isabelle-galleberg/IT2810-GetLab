7ebbc9ccaac33332cc52aadf34964f983a71d2c8
Isabelle Galleberg <isabellegalleberg@gmail.com>
2022-10-04 21:03:57 +0200

    chore: replace type any

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -50,7 +50,7 @@ export default function StartPage() {
     }
   };
 
-  function onConnect(e: any) {
+  function onConnect(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
     if (localProjectId && localAccessToken) {
       localStorage.setItem("projectId", localProjectId);
